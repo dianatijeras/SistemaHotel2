c44d71f4e87e5fa8ec70eca2bb0a8015d7ec068d
Juan Suarez <juanj.suarezm@uqvirtual.edu.co>
2026-09-19 01:05:34 -0500

    feat: creacion del directorio controller con ReservaController

---
+++ backend/src/main/java/co/edu/uniquindio/sistemahotel/backend/controller/ReservaController.java
@@ -0,0 +1,43 @@
+package co.edu.uniquindio.sistemahotel.backend.controller;
+
+import co.edu.uniquindio.sistemahotel.backend.dto.ReservaRequestDTO;
+import co.edu.uniquindio.sistemahotel.backend.dto.ReservaResponseDTO;
+import co.edu.uniquindio.sistemahotel.backend.model.Reserva;
+import co.edu.uniquindio.sistemahotel.backend.service.ReservaService;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class ReservaController {
+
+    private final ReservaService reservaService;
+
+    public ReservaController(ReservaService reservaService) {
+        this.reservaService = reservaService;
+    }
+
+
+    public ReservaResponseDTO registrarReserva(ReservaRequestDTO dto) {
+
+        Reserva reserva = reservaService.crearReserva(dto);
+
+        return ReservaResponseDTO.desde(reserva);
+    }
+
+    public List<ReservaResponseDTO> listarReservas() {
+
+        return reservaService.listarTodas()
+                .stream()
+                .map(ReservaResponseDTO::desde)
+                .toList();
+    }
+
+    public ReservaResponseDTO buscarReserva(String idReserva) {
+
+        Reserva reserva =
+                reservaService.obtenerOFallar(idReserva);
+
+        return ReservaResponseDTO.desde(reserva);
+    }
+}
