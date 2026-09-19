4e912d3e32189a2521b123367df72cda8b6d464b
Juan Suarez <juanj.suarezm@uqvirtual.edu.co>
2026-09-19 01:09:00 -0500

    feat: creacion del repository con las clases ReservaRepository e InMemoryReservaRepository

---
+++ backend/src/main/java/co/edu/uniquindio/sistemahotel/backend/repository/InMemoryReservaRepository.java
@@ -0,0 +1,78 @@
+package co.edu.uniquindio.sistemahotel.backend.repository;
+
+import co.edu.uniquindio.sistemahotel.backend.model.Reserva;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class InMemoryReservaRepository
+        implements ReservaRepository {
+
+    private final Map<String, Reserva> reservas =
+            new ConcurrentHashMap<>();
+
+
+    @Override
+    public List<Reserva> findByNumeroHabitacion(
+            int numeroHabitacion) {
+
+        return reservas.values()
+                .stream()
+                .filter(r ->
+                        r.getHabitacion()
+                                .getNumeroHabitacion()
+                                == numeroHabitacion
+                )
+                .toList();
+    }
+
+
+    @Override
+    public Reserva save(
+            Reserva reserva) {
+
+        reservas.put(
+                reserva.getIdReserva(),
+                reserva
+        );
+
+        return reserva;
+    }
+
+
+    @Override
+    public List<Reserva> findAll() {
+        return List.copyOf(
+                reservas.values()
+        );
+    }
+
+
+    @Override
+    public Optional<Reserva> findById(
+            String idReserva) {
+
+        return Optional.ofNullable(
+                reservas.get(idReserva)
+        );
+    }
+
+
+    @Override
+    public List<Reserva> findByIdHuesped(
+            String idHuesped) {
+
+        return reservas.values()
+                .stream()
+                .filter(r ->
+                        r.getHuesped()
+                                .getIdHuesped()
+                                .equals(idHuesped)
+                )
+                .toList();
+    }
+}
---
+++ backend/src/main/java/co/edu/uniquindio/sistemahotel/backend/repository/ReservaRepository.java
@@ -0,0 +1,27 @@
+package co.edu.uniquindio.sistemahotel.backend.repository;
+
+import co.edu.uniquindio.sistemahotel.backend.model.Reserva;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservaRepository {
+
+    List<Reserva> findAll();
+
+    Optional<Reserva> findById(
+            String idReserva
+    );
+
+    List<Reserva> findByNumeroHabitacion(
+            int numeroHabitacion
+    );
+
+    List<Reserva> findByIdHuesped(
+            String idHuesped
+    );
+
+    Reserva save(
+            Reserva reserva
+    );
+}
